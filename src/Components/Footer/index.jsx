c72b5db10e5b9ee0a9af96da3edf10ee514be114
Yen Xiong Yuan <yenyuan17@gmail.com>
2023-03-21 04:24:36 +0000

    refactor functional components

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+// import React from 'react';
 
-class Footer extends React.Component {
-  render() {
-    return <footer>&copy; 2018</footer>;
-  }
+// class Footer extends React.Component {
+//   render() {
+//     return <footer>&copy; 2018</footer>;
+//   }
+// }
+
+const Footer = () => {
+  return (
+  <footer>&copy; Yen Xiong Yuan</footer>
+  )
 }
 
 export default Footer;
